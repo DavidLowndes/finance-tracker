cb13a2b768fa1e971a9d70957faa2121aa2efccb
David Lowndes <david.lowndes@firstb2b.com>
2016-09-20 13:51:52 +0100

    Add stocks and AJAX for UI (finally) - Lecture 168

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,5 @@
 class StocksController < ApplicationController
-  
+
   def search
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock])
@@ -8,6 +8,7 @@ class StocksController < ApplicationController
     
     if @stock
       #render json: @stock
+      #byebug
       render partial: 'lookup'
     else
       render status: :not_found, nothing: true
